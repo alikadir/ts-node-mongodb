389842378826b0902bd4cf5aa351ff33870dcb36
Ali Kadir BAĞCIOĞLU <alikadirbagcioglu@hotmail.com>
2018-04-08 20:50:10 +0300

    CRUD Faz1

--- dist/index.js
+++ dist/index.js
@@ -1,41 +1,47 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var mongodb_1 = require("mongodb");
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
 var mongoClient = require('mongodb').MongoClient;
 app.use('/img', express_1.default.static(__dirname + '/img'));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.get("/", function (req, res) { return res.sendFile(__dirname + '/index.html'); });
-app.post('/user', function (req, res) {
-    console.log(req);
-    setTimeout(function () {
-        for (var i = 0; i < 999999; i++) {
-            console.log(i);
-        }
-        console.log(Date());
-    }, 100);
-    res.send('ok' + Date());
+var connectionString = 'mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk';
+var db;
+mongodb_1.MongoClient.connect('mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk').then(function (client) {
+    db = client.db('sozluk');
 });
-app.get('/mongo', function (req, res) {
-    mongodb_1.MongoClient.connect('mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk').then(function (client) {
-        var db = client.db('sozluk');
-        db.collection('users').find({}).toArray().then(function (list) {
-            res.send(list);
-        });
+app.get('/users', function (req, res) {
+    db.collection('myUsers').find({}).toArray().then(function (list) {
+        res.send(list);
+    });
+});
+app.get('/users/:id', function (req, res) {
+    db.collection('myUsers').find({ _id: req.params.id }, { _id: 0, name: 1, age: 1 }).limit(1).next().then(function (result) {
+        res.send(result);
+    });
+});
+app.post('/users', function (req, res) {
+    db.collection('myUsers').insertOne(req.body).then(function (ok) { res.send(ok); }).catch(function (err) {
+        console.log(err);
+    });
+});
+app.delete('/users/:id', function (req, res) {
+    db.collection('myUsers').deleteOne({ _id: req.params.id }).then(function (list) {
+        res.status(204).end();
     });
 });
 app.get('/mongo/:query', function (req, res) {
-    mongodb_1.MongoClient.connect('mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk').then(function (client) {
-        var db = client.db('sozluk');
-        var query = JSON.parse(req.params.query);
-        console.log(query);
-        debugger;
-        db.collection('users').find(query).toArray().then(function (list) {
-            res.send(list);
-        });
+    var query = JSON.parse(req.params.query);
+    console.log(query);
+    db.collection('users').find(query).toArray().then(function (list) {
+        res.send(list);
     });
 });
 app.listen(process.env.PORT || 1453, function () { return console.log('server basladi'); });
--- index.ts
+++ index.ts
@@ -1,55 +1,59 @@
 import express from 'express';
-import { MongoClient } from 'mongodb'
-import { Worker } from 'cluster';
+import { MongoClient, Db } from 'mongodb'
+import bodyParser from "body-parser";
 const app = express();
 
 
 const mongoClient = require('mongodb').MongoClient;
 
+
 app.use('/img', express.static(__dirname + '/img'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.post('/user', (req, res) => {
 
-    console.log(req);
-
-    setTimeout(() => {
-        for (let i = 0; i < 999999; i++) {
-            console.log(i);
-        }
-        console.log(Date());
-
-    }, 100);
-
-    res.send('ok' + Date());
+const connectionString = 'mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk';
+var db: Db;
+MongoClient.connect('mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk').then((client) => {
+    db = client.db('sozluk');
+});
 
 
 
-})
 
-app.get('/mongo', (req, res) => {
-    MongoClient.connect('mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk').then((client) => {
-        let db = client.db('sozluk');
-        db.collection('users').find({}).toArray().then(list => {
-            res.send(list);
-        });
+
+app.get('/users', (req, res) => {
+    db.collection('myUsers').find({}).toArray().then(list => {
+        res.send(list);
     });
 });
+
+app.get('/users/:id', (req, res) => {
+    db.collection('myUsers').find({ _id: req.params.id }, {_id:0, name: 1, age: 1 }).limit(1).next().then(result => {
+        res.send(result)
+    });
+});
+
+app.post('/users', (req, res) => {
+    db.collection('myUsers').insertOne(req.body).then(ok => { res.send(ok) }).catch(err => {
+        console.log(err);
+    });
+});
+
+app.delete('/users/:id', (req, res) => {
+    db.collection('myUsers').deleteOne({ _id: req.params.id }).then(list => {
+        res.status(204).end();
+    });
+});
+
 app.get('/mongo/:query', (req, res) => {
-
-    MongoClient.connect('mongodb://Genel:1453@ds041563.mlab.com:41563/sozluk').then((client) => {
-        let db = client.db('sozluk');
-        let query = JSON.parse(req.params.query);
-        console.log(query);
-debugger
-        db.collection('users').find(query).toArray().then(list => {
-            res.send(list);
-        });
+    let query = JSON.parse(req.params.query);
+    console.log(query);
+    db.collection('users').find(query).toArray().then(list => {
+        res.send(list);
     });
-
 });
-
-
-
 
 app.listen(process.env.PORT || 1453, () => console.log('server basladi'));
